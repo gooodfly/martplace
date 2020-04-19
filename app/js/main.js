5d267dd39908ddf1feff039cdb19f6d7c7bfbb78
gooodfly <goodfly@lst.ru>
2020-04-19 23:05:25 +0400

    added slider and products mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
-$(function(){
+$(function () {
 
+    $(".rate-star").rateYo({
+        starWidth: "17px",
+        rating: 4.5,
+        readOnly: true,
+    });
 
+    $('.slider-single__inner').slick({
+        prevArrow: '<button class="slick-arrow slick-prev icon-angle-right"></button>',
+        nextArrow: '<button class="slick-arrow slick-next icon-angle-right"></button>',
 
-    
+    });
+
+    var mixer = mixitup('.product__inner-box');
+
 });
