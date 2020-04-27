36bdf9fcb15b7368818aa911281ed44f692b8942
gooodfly <goodfly@lst.ru>
2020-04-27 23:39:43 +0400

    added index and full adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,39 @@ $(function () {
 
     });
 
+    $('.slider-triple__inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button class="slick-arrow slick-prev icon-angle-right"></button>',
+        nextArrow: '<button class="slick-arrow slick-next icon-angle-right"></button>',
+        responsive: [
+            {
+              breakpoint: 1141,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+              }
+            },
+            {
+                breakpoint: 740,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+        ]
+    });
+
+    $('.header-burger-btn').on('click', function(){
+        $('.header-list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__top-box').toggleClass('active');
+    });
+
+
     var mixer = mixitup('.product__inner-box');
 
 });
